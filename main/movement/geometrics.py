6cf4bab01b6a0be69a5241538c767d9046a3ba77
Gian-Luca Fenocchi <gf220@ic.ac.uk>
2024-05-14 18:13:38 +0100

    Cleaned up code

--- main/movement/geometrics.py
+++ main/movement/geometrics.py
@@ -1,61 +1,53 @@
 import numpy as np
 
 def Rx(roll):
-    """ Rotation matrix arround x (roll)
-    """
-    return np.matrix([[1,            0,             0, 0],
-                      [0, np.cos(roll), -np.sin(roll), 0],
-                      [0, np.sin(roll),  np.cos(roll), 0],
-                      [0,            0,             0, 1]])
+    """Rotation matrix around x (roll)"""
+    c, s = np.cos(roll), np.sin(roll)
+    return np.array([[1, 0,  0, 0],
+                     [0, c, -s, 0],
+                     [0, s,  c, 0],
+                     [0, 0,  0, 1]])
 
 def Ry(pitch):
-    """ Rotation matrix arround y (pitch)
-    """
-#    pitch = np.radians(pitch)
-    return np.matrix([[ np.cos(pitch), 0, np.sin(pitch), 0],
-                      [             0, 1,             0, 0],
-                      [-np.sin(pitch), 0, np.cos(pitch), 0],
-                      [             0, 0,             0, 1]])
+    """Rotation matrix around y (pitch)"""
+    c, s = np.cos(pitch), np.sin(pitch)
+    return np.array([[ c, 0, s, 0],
+                     [ 0, 1, 0, 0],
+                     [-s, 0, c, 0],
+                     [ 0, 0, 0, 1]])
 
 def Rz(yaw):
-    """ Rotation matrix arround z (yaw)
-    """
-    return np.matrix([[np.cos(yaw), -np.sin(yaw), 0, 0],
-                      [np.sin(yaw),  np.cos(yaw), 0, 0],
-                      [          0,            0, 1, 0],
-                      [          0,            0, 0, 1]])
-    
-def Rxyz(roll , pitch , yaw):
-    if roll != 0. or pitch != 0. or yaw != 0.:
-        R = Rx(roll) * Ry(pitch) * Rz(yaw)
-        return R
-    else:
+    """Rotation matrix around z (yaw)"""
+    c, s = np.cos(yaw), np.sin(yaw)
+    return np.array([[c, -s, 0, 0],
+                     [s,  c, 0, 0],
+                     [0,  0, 1, 0],
+                     [0,  0, 0, 1]])
+
+def Rxyz(roll, pitch, yaw):
+    if roll == 0 and pitch == 0 and yaw == 0:
         return np.identity(4)
-    
+    return np.dot(np.dot(Rx(roll), Ry(pitch)), Rz(yaw))
 
 def RTmatrix(orientation, position):
-    """compose translation and rotation"""
-    roll = orientation[0]
-    pitch = orientation[1]
-    yaw = orientation[2]
-    x0 = position[0]
-    y0 = position[1]
-    z0 = position[2]
+    """Compose translation and rotation"""
+    roll, pitch, yaw = orientation
+    x0, y0, z0 = position
     
-    translation = np.matrix([[1, 0, 0, x0],
-                             [0, 1, 0, y0],
-                             [0, 0, 1, z0],
-                             [0, 0, 0,  1]])
+    translation = np.array([[1, 0, 0, x0],
+                            [0, 1, 0, y0],
+                            [0, 0, 1, z0],
+                            [0, 0, 0, 1]])
+    
     rotation = Rxyz(roll, pitch, yaw)
-    
-    return rotation*translation
-    
-def transform(coord,rotation,translation):
-    """transforms a vector to a desire rotation and translation"""
+    return np.dot(rotation, translation)
+
+def transform(coord, rotation, translation):
+    """Transforms a vector to a desired rotation and translation"""
     vector = np.array([[coord[0]],
                        [coord[1]],
                        [coord[2]],
-                       [      1]])
+                       [1]])
     
-    tranformVector = RTmatrix(rotation, translation) * vector
-    return np.array([tranformVector[0,0], tranformVector[1,0], tranformVector[2,0]])
+    transform_vector = np.dot(RTmatrix(rotation, translation), vector)
+    return transform_vector[:3, 0]
